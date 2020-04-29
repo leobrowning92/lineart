e7f9f82a53a60727deb5c9916800b50f0ba50dbc
Leo Browning <leo.browning@qrious.co.nz>
2020-04-29 22:12:44 +1200

    pulled some shapes and functions out of the notebook to lighten it up

---
+++ lineart/shapes.py
@@ -0,0 +1,43 @@
+import numpy as np
+from lineart.transforms import rotate
+
+class Octahedron:
+    def __init__(self, center, radius):
+        origin_nodes = np.array([
+            [0,  radius, 0],
+            [-radius, 0, 0],
+            [0, 0, -radius],
+            [ radius, 0, 0],
+            [0, 0,  radius],
+            [0, -radius, 0],
+        ])
+        self.nodes = origin_nodes + center
+        self.edges = self.edges_from_nodes()
+    def edges_from_nodes(self):
+        edges = np.array([
+            [self.nodes[0], self.nodes[1]],
+            [self.nodes[0], self.nodes[2]],
+            [self.nodes[0], self.nodes[3]],
+            [self.nodes[0], self.nodes[4]],
+            [self.nodes[5], self.nodes[1]],
+            [self.nodes[5], self.nodes[2]],
+            [self.nodes[5], self.nodes[3]],
+            [self.nodes[5], self.nodes[4]],
+            [self.nodes[1], self.nodes[2]],
+            [self.nodes[2], self.nodes[3]],
+            [self.nodes[3], self.nodes[4]],
+            [self.nodes[4], self.nodes[1]],
+            
+        ])
+        return edges
+    
+    def rotate(self, p0, normal, theta):
+        self.nodes = rotate(self.nodes, p0, normal, theta)
+        self.edges = self.edges_from_nodes()
+        
+    def sample(self, n, dither):
+        line_points = [sample(*e, dither) for e in self.edges]
+        points = np.concatenate(line_points)
+        return points
+        
+        
